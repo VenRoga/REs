87075de81d6c12afc1fce8f222fb07942b95681b
VenRoga <frfeyndcnbv@mail.ru>
2026-06-07 17:34:24 +0300

    Добавьте файлы проекта.

---
+++ REs/App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace REs
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new AppShell();
+        }
+    }
+}
---
+++ REs/AppShell.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace REs
+{
+    public partial class AppShell : Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+        }
+    }
+}
